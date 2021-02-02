2ca143bb3f7f381f8ea79bf70eb3d53bfc7f44c5
Margarida Salvado <margaridasalvado@MacBook-Pro-de-Margarida.local>
2021-02-02 16:33:24 +1100

    final tweaks

--- custom.js
+++ custom.js
@@ -1,57 +1,57 @@
-(function($) {
+
 const cardsOnce = [
      {
 			name: "me-1",
-			img: "http://waddlewaddle.com.au/wp-content/uploads/2021/02/me-pool.png",
+			img: "https://waddlewaddle.com.au/wp-content/uploads/2021/02/me-surf.png",
 			id: 1,
 		},
 		{
 			name: "me-2",
-			img: "http://waddlewaddle.com.au/wp-content/uploads/2021/02/me-white.png",
+			img: "https://waddlewaddle.com.au/wp-content/uploads/2021/02/me1.png",
 			id: 2
 		},
 		{
 			name: "me-3",
-			img: "http://waddlewaddle.com.au/wp-content/uploads/2021/02/croissant.png",
+			img: "https://waddlewaddle.com.au/wp-content/uploads/2021/02/me-jump.png",
 			id: 3
 		},
 		{
 			name: "me-4",
-			img: "http://waddlewaddle.com.au/wp-content/uploads/2021/02/me-jump.png",
+			img: "https://waddlewaddle.com.au/wp-content/uploads/2021/02/me-pool.png",
 			id: 4
 		},
 		{
 			name: "me-5",
-			img: "http://waddlewaddle.com.au/wp-content/uploads/2021/02/me1.png",
+			img: "https://waddlewaddle.com.au/wp-content/uploads/2021/02/croissant.png",
 			id: 5
 		},
 		{
 			name: "me-6",
-			img: "http://waddlewaddle.com.au/wp-content/uploads/2021/02/me-surf.png",
+			img: "https://waddlewaddle.com.au/wp-content/uploads/2021/02/me-white.png",
 			id: 6
 		},
 		{
 			name: "josh",
-			img: "http://waddlewaddle.com.au/wp-content/uploads/2021/02/josh.png",
+			img: "https://waddlewaddle.com.au/wp-content/uploads/2021/02/josh.png",
 			id: 7
 		},
 		{
 			name: "mano",
-			img: "http://waddlewaddle.com.au/wp-content/uploads/2021/02/mano3.png",
+			img: "https://waddlewaddle.com.au/wp-content/uploads/2021/02/mano2.png",
 			id: 8
 		},
 		{
 			name: "mano-lady",
-			img: "http://waddlewaddle.com.au/wp-content/uploads/2021/02/mano2.png",
+			img: "https://waddlewaddle.com.au/wp-content/uploads/2021/02/mano3.png",
 			id: 9
 		},
 		{
 			name: "mano-eu",
-			img: "http://waddlewaddle.com.au/wp-content/uploads/2021/02/Sem-título-1.png",
+			img: "https://waddlewaddle.com.au/wp-content/uploads/2021/02/Sem-título-1.png",
 			id: 10
 		}
 ];
-    var cardArray = $.merge(cardsOnce, cardsOnce);
+    var cardArray = jQuery.merge(cardsOnce, cardsOnce);
     let grid = document.querySelector(".grid");
     /* These variables can be used if a play again pop up and a scoreboard/clickboard are to be used
     let scoreBoard = document.querySelector(".scoreBoard");
@@ -84,7 +84,7 @@ const cardsOnce = [
             card.innerHTML = '<div class="inside">\
 				<div class="front"><img src="'+ cardArray[index].img +'"\
 				alt="'+ cardArray[index].name +'" /></div>\
-				<div class="back"><img src="http://waddlewaddle.com.au/wp-content/uploads/2021/02/cover.svg"\
+				<div class="back"><img src="https://waddlewaddle.com.au/wp-content/uploads/2021/02/cover.svg"\
 				alt="card down" /></div></div>';
             grid.appendChild(card);
 })
@@ -103,7 +103,7 @@ const cardsOnce = [
 
     // flip Card function
     function flipCard() {
-        let selected = $(this).attr("id");
+        let selected = jQuery(this).attr("id");
         cardsSelected.push(cardArray[selected].name);
         cardsId.push(selected);
         this.classList.add("flip");
@@ -111,7 +111,7 @@ const cardsOnce = [
         if (cardsId.length === 2) {
             setTimeout(checkForMatch, 500);
                 for (var i = 0 ; i < cardsId.length; i++) {
-                        $(cardsId[i]).addClass("picked");
+                        jQuery(cardsId[i]).addClass("picked");
 
             }
             }
@@ -124,7 +124,7 @@ const cardsOnce = [
             let secondCard = cardsId[1];
             if (cardsSelected[0] === cardsSelected[1] && firstCard == secondCard) {
                 let flipped =  document.getElementsByClassName("flip");
-                 $('#match-img-pop').show();
+                 jQuery('#match-img-pop').show();
                     cardsWon += 1;
                 for (var i = 0 ; i < flipped.length; i++) {
                     flipped[i].classList.add("matched");
@@ -135,7 +135,7 @@ const cardsOnce = [
 
             } else {
                 for (var i = 0 ; i < imgs.length; i++) {
-                    if ($(imgs[i]).hasClass( "flip" ) && !$(imgs[i]).hasClass("matched")) {
+                    if (jQuery(imgs[i]).hasClass( "flip" ) && !jQuery(imgs[i]).hasClass("matched")) {
                         imgs[i].classList.remove("flip");
                         }
 
@@ -148,14 +148,14 @@ const cardsOnce = [
     }
 
     function hidePopUpImg() {
-        if ($('.memory-game-pop-imgs').is(':visible')) {
-            $('.memory-game-pop-imgs').hide();
+        if (jQuery('.memory-game-pop-imgs').is(':visible')) {
+            jQuery('.memory-game-pop-imgs').hide();
         }
     }
 
     function checkWon() {
         if (cardsWon == cardArray.length / 2) {
-            setTimeout(()=> $('#win-img-pop').show(),300);
+            setTimeout(()=> jQuery('#win-img-pop').show(),300);
 
            // setTimeout(hidePopUpImg, 2000);
             //setTimeout(()=> popup.style.display = "flex" ,300);
@@ -175,4 +175,3 @@ const cardsOnce = [
         scoreBoard.innerHTML = 0; */
         popup.style.display = "none";
     }
-})( jQuery );
